86e4c6e6d1fcb3659f7e7d83c39f19d9395444bb
Itay Raz <itay@studioraz.co.il>
2022-03-01 19:08:57 +0200

    print different additional info params on block info based on area code (adminhtml, frontend)

--- Block/Info.php
+++ Block/Info.php
@@ -4,6 +4,7 @@ namespace SR\Cardcom\Block;
 
 use Magento\Framework\Phrase;
 use Magento\Payment\Block\ConfigurableInfo;
+use Magento\Framework\App\Area;
 
 class Info extends ConfigurableInfo
 {
@@ -13,8 +14,26 @@ class Info extends ConfigurableInfo
      * @param string $field
      * @return Phrase
      */
-    protected function getLabel($field)
+    protected function getLabel($field): Phrase
     {
         return __($field);
+    }
+
+    /**
+     * Check whether payment information should show up in secure mode
+     * true => only "public" payment information may be shown
+     * false => full information may be shown
+     *
+     * @return bool
+     * @SuppressWarnings(PHPMD.BooleanGetMethodName)
+     */
+    public function getIsSecureMode(): bool
+    {
+
+        if ($this->getArea() == Area::AREA_ADMINHTML) {
+            return false;
+        }
+
+        return parent::getIsSecureMode();
     }
 }
